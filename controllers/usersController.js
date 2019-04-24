2702f428b4d5bdbb61f5ac2449cc4b23a1155d5a
Porter Truax <portertruax@Porters-MacBook-Pro.local>
2019-04-24 14:31:45 -0500

    login page rendering

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+//GET LOGIN PAGE
+
+router.get('/login', async (req, res) => {
+	res.render('login.ejs')
+})
 
 //exportRouter
 module.exports = router
