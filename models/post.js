070896a86cd68a6a6bf76259357a59fcac179cb4
Porter Truax <portertruax@Porters-MacBook-Pro.local>
2019-04-24 13:08:02 -0500

    added post schema

---
+++ models/post.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Comment = require('./comment')
+
+const postSchema = new mongoose.Schema({
+	name: {type: String, required: true},
+	date: {type: Date},
+	body: {type: String, required: true},
+	comments: [{
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Comment'
+	}]
+})
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
