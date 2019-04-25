9556defe406559fa84819d77678c8f7ccf864c9b
Porter Truax <portertruax@Porters-MacBook-Pro.local>
2019-04-25 11:13:17 -0500

    create new post form rendering

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -5,8 +5,27 @@ const User = require('../models/user')
 const Topic = require('../models/topic')
 const Post = require('../models/post')
 
-router.get('/new', (req,res) => {
-	res.send('new route working')
+router.get('/new', async (req,res) => {
+
+	const findAllTopics = await Topic.find({})
+
+	res.render('posts/new.ejs', {
+		topics: findAllTopics
+	})
+})
+
+router.post('/', async (req,res) => {
+
+	const createdPost = await Post.create(req.body)
+
+	try {
+
+
+	} catch(err) {
+		res.send(err)
+	}
+
+
 })
 
 
