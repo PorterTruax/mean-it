bc7af8f3a0c0be64e65b5a75b7ca7575fe883576
Porter Truax <portertruax@Porters-MacBook-Pro.local>
2019-04-25 16:29:52 -0500

    comment route working

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user')
+const Topic = require('../models/topic')
+const Post = require('../models/post')
+
+router.post('/', async(req,res) => {
+	res.send("Test")
+})
+
 
 
 //exportRouter
