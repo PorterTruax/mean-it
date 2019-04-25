70cc78511f82baa1c118ecc2d084dae12dc3f164
Porter Truax <portertruax@Porters-MacBook-Pro.local>
2019-04-25 10:49:28 -0500

    create new post route working

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user')
+const Topic = require('../models/topic')
+const Post = require('../models/post')
+
+router.get('/new', (req,res) => {
+	res.send('new route working')
+})
+
 
 
 //exportRouter
