708ee142c2522c834ab5505b18b42015ca85205c
Porter Truax <portertruax@Porters-MacBook-Pro.local>
2019-04-25 14:57:28 -0500

    delete/edit posts working

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -5,10 +5,90 @@ const User = require('../models/user')
 const Topic = require('../models/topic')
 const Post = require('../models/post')
 
-//GET ROUTE FOR POSTS SHOW PAGE
 
+
+//GET ROUTE FOR POSTS EDIT PAGE
+router.get('/:id/edit', async (req,res) => {
+	try{
+		const foundPost = await Post.findById(req.params.id)
+		const foundUser = await User.findById(req.session.usersDbId)
+
+		res.render('posts/edit.ejs', {
+			post: foundPost,
+			user: foundUser
+		})
+
+	}catch(err) {
+		res.send(err)
+	}
+})
+
+router.put('/:id', async(req,res) => {
+	const updatedName = req.body.name
+	const updatedDate = req.body.date
+	const updatedBody = req.body.body
+
+	try{
+		const foundPost = await Post.findById(req.params.id)
+
+		foundPost.name = updatedName
+
+		foundPost.date = updatedDate
+
+		foundPost.body = updatedBody
+
+		const savedPost = await foundPost.save()
+
+		res.redirect('/posts/'  + req.params.id)
+
+	} catch(err) {
+
+		res.send(err)
+	}
+
+})
+
+//DELETE ROUTE
+
+router.delete('/:id', async(req,res) => {
+	try {
+
+		const foundPost = await Post.findByIdAndRemove(req.params.id)
+
+		// const foundUser = await User.findOne({_id: req.session.usersDbId})
+
+		const foundTopic = await Topic.findOne({'posts': req.params.id})
+
+		await foundTopic.posts.remove(req.params.id)
+
+		await foundTopic.save()
+
+		res.redirect('/topics/' + foundTopic._id);
+
+	}catch(err) {
+		res.send(err)
+	}
+})
+
+
+//GET ROUTE FOR POSTS SHOW PAGE
 router.get('/:id', async (req,res) => {
-	res.send('test')
+	try {
+
+		const foundPost = await Post.findById(req.params.id)
+		const foundUser = await User.findById(req.session.usersDbId)
+
+		console.log("\n", "here is the found post\n")
+		console.log(foundPost);
+
+		res.render('posts/show.ejs', {
+			post: foundPost,
+			user: foundUser
+		})
+
+	}catch(err) {
+		res.send(err)
+	}
 })
 
 
